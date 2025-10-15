5912ce2a7be2799336bf6407b99bf4d6ff337f93
ian9494 <ian20050904@gmail.com>
2025-10-15 23:54:57 +0800

    20251015-5.2
    找user id的bug

--- src/routes/snapshot.js
+++ src/routes/snapshot.js
@@ -10,6 +10,7 @@ router.get('/snapshot', async (req, res) => {
     // 支援 query 參數 user_id/opaque_user_id
     const { user_id, opaque_user_id } = req.query;
     const userKey = userKeyOf({ user_id, opaque_user_id });
+    console.log('[snapshot] user_id:', user_id, 'opaque_user_id:', opaque_user_id, '=> userKey:', userKey);
     const result = await getSnapshot(userKey);
     res.json(result);
 });
